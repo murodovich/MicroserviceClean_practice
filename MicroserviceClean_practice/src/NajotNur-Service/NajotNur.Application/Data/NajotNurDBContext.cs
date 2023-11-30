cdc88390393a1ba98f9106ddce3d2ee4bc7da9ea
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-30 23:52:57 +0500

    dada

---
+++ MicroserviceClean_practice/src/NajotNur-Service/NajotNur.Application/Data/NajotNurDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NajotNur.Domain.Entities.Models;
+
+namespace NajotNur.Application.Data
+{
+    public class NajotNurDBContext : DbContext
+    {
+        public NajotNurDBContext(DbContextOptions<NajotNurDBContext> options) : base(options)
+        {
+            
+        }
+         
+        public DbSet<User> users { get; set; }
+
+     }
+}
