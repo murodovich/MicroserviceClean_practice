68c5641de91746e83824598615371956957c0ab3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-30 23:53:41 +0500

    dadad

---
+++ MicroserviceClean_practice/src/NajotNur-Service/NajotNur.Infrastructure/Services/UserServices/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using NajotNur.Domain.Entities.Models;
+using NajotNur.Infrastructure.Dtos;
+
+namespace NajotNur.Infrastructure.Services.UserServices
+{
+    public interface IUserService : IBaseService<User,UserDto>
+    {
+
+    }
+}
