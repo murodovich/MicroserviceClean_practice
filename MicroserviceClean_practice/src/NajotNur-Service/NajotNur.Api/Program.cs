db510cab7440363d3f244f136c6286cb962ae1b6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-30 23:52:53 +0500

    dadsad

---
+++ MicroserviceClean_practice/src/NajotNur-Service/NajotNur.Api/Program.cs
@@ -0,0 +1,35 @@
+using Microsoft.EntityFrameworkCore;
+using NajotNur.Application.Data;
+using NajotNur.Application.Repositories.UserRepositories;
+using NajotNur.Infrastructure.Services.UserServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<NajotNurDBContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+    
+});
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
