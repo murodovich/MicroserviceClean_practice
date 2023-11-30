1757ef6f9c4fe99c1b2a4e4fabdae718dbedbc60
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-30 23:53:27 +0500

    dadsa

---
+++ MicroserviceClean_practice/src/NajotNur-Service/NajotNur.Domain/Entities/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace NajotNur.Domain.Entities.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
