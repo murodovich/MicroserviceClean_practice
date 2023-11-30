9ca68b778aa86723df19610df283fc32457d6bb2
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-30 23:53:18 +0500

    adad

---
+++ MicroserviceClean_practice/src/NajotNur-Service/NajotNur.Application/Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace NajotNur.Application.Repositories
+{
+    public interface IBaseRepository<TModel> where TModel : class
+    {
+        public ValueTask<int> CreateAsync(TModel user);
+        public ValueTask<int> UpdateAsync(int Id, TModel user);
+        public ValueTask<int> DeleteAsync(int id);
+        public ValueTask<IList<TModel>> GetAllAsync();
+        public ValueTask<TModel> GetByIdAsync(int id);
+
+    }
+}
