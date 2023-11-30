2a1609e822b256a0808b27e5e4f1a744a73c6807
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-11-30 23:53:49 +0500

    dadad

---
+++ MicroserviceClean_practice/src/NajotNur-Service/NajotNur.Infrastructure/Services/IBaseService.cs
@@ -0,0 +1,13 @@
+﻿namespace NajotNur.Infrastructure.Services
+{
+    public interface IBaseService<Tmodel,TDto> where Tmodel : class
+    {
+        public ValueTask<int> Create(TDto dto);
+        public ValueTask<int> Update(int id, TDto dto);
+        public ValueTask<int> Delete(int id);
+        public ValueTask<IList<Tmodel>> GetAll();
+        public ValueTask<Tmodel> GetById(int id);
+
+
+    }
+}
